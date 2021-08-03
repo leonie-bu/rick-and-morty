b11dcdb539cd27d17821d398bf3015f30327ef6a
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-03 12:32:33 +0200

    🎨 Append title and input in one line
    Append title and input in one line

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ const mainElement = document.createElement('main');
 const title = document.createElement('h1');
 title.innerText = 'Rick and Morty';
 
-mainElement.append(title);
-
 const input = document.createElement('input');
 input.placeholder = 'Enter Text';
+
+mainElement.append(title, input); // appenden, damit es auf Seite sichtbar wird
 
 if (app !== null) {
   app.append(mainElement);
