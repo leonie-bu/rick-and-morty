0116e3f027a402a232ec5141195797716b10b2ff
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-03 14:43:03 +0200

    Add and adapt createElement function

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,30 @@
 import './style.css';
+import { createElement } from './utils/createElement';
+
+const mainElement = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerText: 'Rick and Morty',
+      className: 'app__title',
+    }),
+    createElement('input', {
+      placeholder: 'Enter Text',
+      className: 'app__input',
+    }),
+  ],
+});
 
 const app = document.querySelector<HTMLDivElement>('#app');
-
-const mainElement = document.createElement('main');
-
-const title = document.createElement('h1');
-title.innerText = 'Rick and Morty';
-title.className = 'app__title';
-
-const input = document.createElement('input');
-input.placeholder = 'Enter Text';
-input.className = 'app__input';
-
-mainElement.append(title, input); // appenden, damit es auf Seite sichtbar wird
 
 if (app !== null) {
   app.append(mainElement);
 }
+
+// const mainElement = document.createElement('main');
+// const title = document.createElement('h1');
+// title.innerText = 'Rick and Morty';
+// title.className = 'app__title';
+// const input = document.createElement('input');
+// input.placeholder = 'Enter Text';
+// input.className = 'app__input';
+// mainElement.append(title, input); // appenden, damit es auf Seite sichtbar wird
