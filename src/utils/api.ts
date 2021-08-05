fb5f65040bfc3bda6149cf7ba447f17f223b4556
leonie-bu <leonie.buschbeck@gmail.com>
2021-08-05 16:17:02 +0200

    ✨ Edit and update getCharacters function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,17 @@
-export async function getCharacters() {
+import { AllCharactersFromAPI, Character } from '../types';
+
+export async function getCharacters(): Promise<Character[]> {
   const response = await fetch('https://rickandmortyapi.com/api/character');
-  const data = await response.json();
-  const character = data.results;
-  return character;
+  const data: AllCharactersFromAPI = await response.json();
+  const characters = data.results;
+  const formattedCharacters: Character[] = characters.map((character) => {
+    const formattedCharacter: Character = {
+      name: character.name,
+      status: character.status,
+      location: character.location.name,
+      thumbnail: character.image,
+    };
+    return formattedCharacter;
+  });
+  return formattedCharacters;
 }
